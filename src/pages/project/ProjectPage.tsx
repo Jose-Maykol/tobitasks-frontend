8a8a1285d4b2f93843b5d84780e5be897aa89b5e
Jose-Maykol <jpaniura@unsa.edu.pe>
2025-03-17 23:45:45 -0500

    feat: implement project data fetching and loading state in project detail page

--- src/pages/project/ProjectPage.tsx
+++ src/pages/project/ProjectPage.tsx
@@ -1,8 +1,32 @@
 /* import TaskBoardContainer from './TaskBoardContainer' */
+import { useQuery } from 'react-query'
 import ProjectHeader from './ProjectHeader'
 import KanbanBoard from './components/KanbanBoard'
+import { useParams } from 'react-router-dom'
+import { projectService } from '@/services/ProjectService'
+import useProjectStore from '@/stores/useProjectStore'
 
 function ProjectPage (): JSX.Element {
+  const { id } = useParams()
+  const { setProject } = useProjectStore()
+  const { data, isLoading } = useQuery('project', async () => {
+    if (id === undefined) return
+    const project = await projectService.getById(id)
+    return project.data
+  }, {
+    refetchOnWindowFocus: false,
+    retry: 1,
+    onSuccess: (data) => {
+      if (data !== undefined) {
+        setProject(data)
+      }
+    }
+  })
+
+  if (isLoading && data === undefined) {
+    return <div>Loading...</div>
+  }
+
   return (
     <section className='flex flex-col w-full h-full'>
       <ProjectHeader />
